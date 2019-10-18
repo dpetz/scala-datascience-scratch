35b5cb7e7c5e55e5ab1579268e9fe3d67ee0f47a
dpetzoldt <Dirk.Petzoldt@zalando.de>
2019-10-18 23:59:42 +0200

    Rollback Expr as a Product type

--- src/main/scala/ds/test/Test.scala
+++ src/main/scala/ds/test/Test.scala
@@ -10,41 +10,46 @@ object Test {
   type E[T] = Expr[T]
 
   trait Engine {
-    def apply[T](e:E[T]):T
+    def scalar[T](e:E[T]):T
+    def vec[T](e:E[T]):Seq[T]
   }
 
   /** Monadic with ``Expr.apply`` as unit and  ``andThen`` as flatMap */
-  case class Expr[T](eval:Engine=>T) {
+  case class Expr[T](eval:Engine=>Seq[T]) {
     //def eval(e:Engine):X
-    def **[Y,Z](y:Expr[Y])(implicit op:Power[T,Y,Z]):Expr[Z] = op(this,y)
+    def **(y:E[T])(implicit op:Power[T]):E[T] = map(op(_,y))
 
+    def *[X,Y,Z](y:E[Y])(implicit op:Times[X,Y,Z]):E[Z] = op(this,y)
 
-    def *[X,Y,Z](y:Expr[Y])(implicit op:Times[X,Y,Z]):Expr[Z] = op(this,y)
-
-    def /[X,Y,Z](y:Expr[Y])(implicit op:Div[X,Y,Z]):Expr[Z] = op(this,y)
+    def /[X,Y,Z](y:E[Y])(implicit op:Div[X,Y,Z]):E[Z] = op(this,y)
 
     /** flatMap */
-    def andThen[T](f:T=>E[T]): E[T] = Expr {
+    def andThen[S](f:T=>E[S]): E[S] = Expr {
       e => e(f(e(this)))
     }
 
-    //def map[X](implicit vec:Vec[T,X])
+    def map[S](f:E[T]=>E[S]):E[T]
+    def map[S](f:T=>S):E[T]
+
 
   }
 
+  /*
   object Expr {
-    //def const[T](t:T):E[T] = expr(t)
+    def const[T](t:T):E[T] = expr(t)
   }
+   */
 
   /** Sequence S of Elements X  */
   trait Vec[T,X]
 
-  trait Power[X,Y,Z] extends ((E[X],E[Y])=>E[Z])
+  trait Power[R] extends ((R,R)=>R)
 
-  implicit def realPower[R](implicit real:Real[R]):Power[R,R,R] =
-    (x,p) => Expr { e => real.power(e(x),e(p)) }
+  implicit def realPower[R](implicit real:Real[R]):Power[R] = real.power
 
-  implicit def expr[T](t:T): E[T] = Expr { _ => t }
+    // (x,p) => Expr { e => e.vec(x.liftmap(real.power(_,e.scalar(p)))) }
+
+  //implicit def expr[T](t:T): E[T] = Expr { _ => List(t) }
 
 
 
@@ -62,16 +67,20 @@ object Test {
     (v:Seq[R],w:Seq[R]) => (v zip w) map (x => real.plus(x._1,x._2))
 
   def lift[X1,X2,Y](f:(X1,X2)=>Y):(E[X1],E[X2])=>E[Y] =
-    (e1, e2) => Expr {e => f(e(e1), e(e2)) }
+    (e1, e2) => Expr {e => List(f(e.scalar(e1), e.scalar(e2))) }
+
+  def lift[X,Y](f:X=>Y):E[X]=>E[Y] =
+    (e1) => Expr {e => List(f(e.scalar(e1))) }
 
   //def :*:[R:Real](v:E[Seq[R]], w:E[Seq[R]]):E[Seq[R]] = lift(elementwiseTimes)(v,w)
 
   def sum[R](v:E[Seq[R]])(implicit real:Real[R]):E[R] =
     Expr { e=> e(v).foldLeft(real.zero)(real.plus) }
 
+  /*
   def map[R,S](v:E[Seq[R]],f:R=>Expr[S]):E[Seq[S]] =
     Expr { e => e(e(v) map f) }
-
+*/
 
   def dot[R:Real](v:E[Seq[R]]): E[R] = sum(v*v)
 
@@ -85,8 +94,8 @@ object Test {
 */
 
   def norm[R](p:Int)(v:E[Seq[R]])(implicit real:Real[R]):Expr[R] =  p match {
-    case real.one => sum ( map (v, real.abs) )
-    case p:_ => sum( map(v, expr(_) ** real(p) )) ** real.one./(real(p))
+    case real.one => sum (v.map(real.abs) )
+    case p:_ => sum( v.map( x => expr(x) ** real(p) )) ** real.one./(real(p))
 
   }
 
